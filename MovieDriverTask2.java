21ef7eb34f479de38a6dd512e78e055f09f5c620
Mr. Alexander Zlodorev <azlodore@montgomerycollege.edu>
2026-09-21 17:42:10 -0400

    Update MovieDriverTask2.java

--- MovieDriverTask2.java
+++ MovieDriverTask2.java
@@ -1,24 +1,28 @@
 import java.util.Scanner;
-
+/** This program reads input from the keyboard about a movie and uses Movie class object to store these values.
+ */
 public class MovieDriverTask2 {
 
 	public static void main(String[] args) {
+		//Objects used in the program, as well as several variables
 		Scanner input = new Scanner(System.in);
 		Movie movie=new Movie();
 		char answer = 'y';
 		String inputS;
 		do {
+
+			//Reads the title of the movie and sets the title field in movie to that value
 			System.out.println("Enter the title of the movie: ");
 			movie.setTitle(input.nextLine());
-			//input.nextLine();
+			//Reads the rating of the movie and sets the rating field in movie to that value
 			System.out.println("Enter the rating of the movie: ");
 			movie.setRating(input.nextLine());
-			
+			//Reads the number of tickets sold and sets the soldTickers field in movie to that value
 			System.out.println("Enter the number of tickets sold for this movie: ");
 			movie.setSoldTickets(input.nextInt());
-			input.nextLine();
-			System.out.println(movie.toString());
-			
+			//Displays the information about the movie
+			System.out.print(movie.toString());
+			//Ask the user whether he wants to continue and evaluate the choice
 			System.out.print("Want to enter another movie? (Y/N): ");
 			inputS = input.nextLine();
 			answer = inputS.charAt(0);
@@ -28,3 +32,4 @@ public class MovieDriverTask2 {
 	}
 
 }
+
